8fff509d1b563eb42fa2cec2cee037cdb6e85c8d
pauldivaniii <pauldivaniii@cox.net>
2016-05-11 07:56:20 -0700

    Initial Creation of REST API projects

---
+++ Homework/HomeworkRest/Controllers/PersonsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeworkRest.Controllers
+{
+    public class PersonsController : ApiController
+    {
+    }
+}
